4fa329cc9ab65c9e42078bd1bbf6c66816e18524
k-ola <olakrusiec@gmail.com>
2022-02-09 18:43:45 +0100

    version with the list with all pois clickable preliminary db.poi details added. no pictures displyed

--- app/src/main/java/com/example/wbunkry/activities/SinglePoiActivity.kt
+++ app/src/main/java/com/example/wbunkry/activities/SinglePoiActivity.kt
@@ -1,12 +1,21 @@
 package com.example.wbunkry.activities
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.wbunkry.R
 import com.example.wbunkry.database.Pois
 import com.example.wbunkry.database.ShortPathDB
+import java.io.IOException
+import java.io.InputStream
 
 class SinglePoiActivity : AppCompatActivity() {
     private  lateinit var poi: Pois
@@ -17,9 +26,24 @@ class SinglePoiActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val position = intent.getIntExtra("position", 0)
         poi = ShortPathDB.poiList.get(position)
-       setContentView(R.layout.single_poi)
+        setContentView(R.layout.single_poi)
+
+        val picName = poi.img
+        val bitmap: Bitmap? = assetsToBitmap(picName)
+        bitmap?.apply {
+           findViewById<ImageView>(R.id.imgPoi).setImageBitmap(this)
+        }
 
 
+
+        findViewById<ImageView>(R.id.imgPoi).setImageAssets(applicationContext,picName)
+
+
+
+       /*
+       val imgPoi = findViewById<ImageView>(R.id.imgPoi)
+        imgPoi.setImageResource(R.drawable.ma27)
+*/
         val namePoi = findViewById<TextView>(R.id.namePoi)
         namePoi.text=poi.name
 
@@ -41,4 +65,22 @@ class SinglePoiActivity : AppCompatActivity() {
 
 
 
+}
+
+fun ImageView.setImageAssets(context: Context, picName: String) {
+    try {
+        with(context.assets.open(picName)) {
+            setImageBitmap(BitmapFactory.decodeStream(this))
+        }
+    } catch (e: IOException) {
+
+    }
+}
+
+fun Context.assetsToBitmap(picName: String): Bitmap? {
+    return try {
+        with(assets.open(picName)) {
+            BitmapFactory.decodeStream(this)
+        }
+    }catch (e: IOException) {null}
 }
--- app/src/main/java/com/example/wbunkry/database/ShortPathDB.kt
+++ app/src/main/java/com/example/wbunkry/database/ShortPathDB.kt
@@ -5,14 +5,15 @@ import android.graphics.Point
 object ShortPathDB {
 
     val poiList: ArrayList<Pois> = arrayListOf<Pois>(
-        Pois("poi1","59.888888","18.656989","desc1"),
-        Pois("poi2","59.786700","18.569856","desc2"),
-        Pois("poi3","58.888901", "18.482521","desc3")
+        Pois("ma27.png","poi1","59.888888","18.656989","ddesc desc desc desc"),
+        Pois("tlo.jpg","poi2","59.786700","18.569856","desc2"),
+        Pois("tlo1b.jpg","poi3","58.888901", "18.482521","desc3")
 
             )
 }
 
 data class Pois(
+    val img: String,
     val name: String,
     val lat: String,
     val lng: String,
